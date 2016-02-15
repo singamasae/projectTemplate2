1013d938eb3fe7e8c4233b869a70212f3d42b1c8
Administrator <gderudyartha@gmail.com>
2016-02-15 10:11:29 +0800

    update session timeout

--- src/main/java/com/project/template/services/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/project/template/services/security/CustomAuthenticationEntryPoint.java
@@ -6,16 +6,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
-	private static Logger log = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
+	//private static Logger log = LoggerFactory.getLogger(CustomAuthenticationEntryPoint.class);
 
 	public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-		log.error(authException.getMessage(), authException);
+		//log.error(authException.getMessage(), authException);
 		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Authentication token was either missing or invalid.");
 	}
 
